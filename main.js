7f5be3a2d0f08f57ec1e9bae7447c5e1a7b0d3f0
ozaki <fs65ugumori@yahoo.co.jp>
2019-06-25 00:09:05 +0900

    add script

---
+++ main.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+const baseUrl = process.env.BASE_URL;
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(baseUrl, { waitUntil: 'networkidle0' });
+  await page.waitFor(3000);
+
+  await page.goto(`${baseUrl}/admin`, { waitUntil: 'networkidle2' });
+  await page.evaluate(() => document.querySelector('#execute').click());
+  await page.waitFor(3000);
+
+  await browser.close();
+})();
