427d73711bd11261b1f3465267f556bad78706e3
ozaki <fs65ugumori@yahoo.co.jp>
2019-06-25 00:11:49 +0900

    update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const baseUrl = process.env.BASE_URL;
   const page = await browser.newPage();
 
   await page.goto(baseUrl, { waitUntil: 'networkidle0' });
-  await page.waitFor(3000);
+  await page.waitFor(2000);
 
   await page.goto(`${baseUrl}/admin`, { waitUntil: 'networkidle2' });
   await page.evaluate(() => document.querySelector('#execute').click());
